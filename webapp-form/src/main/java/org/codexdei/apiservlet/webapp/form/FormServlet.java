a58842f68f4b1b12c0699a6de0a3e38fbc23ca3c
codexdei <jlacostc@gmail.com>
2025-06-05 16:49:20 -0500

    se agregan validadores para las variables del servlet

--- webapp-form/src/main/java/org/codexdei/apiservlet/webapp/form/FormServlet.java
+++ webapp-form/src/main/java/org/codexdei/apiservlet/webapp/form/FormServlet.java
@@ -8,7 +8,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @WebServlet("/registration")
 public class FormServlet extends HttpServlet {
@@ -16,6 +18,7 @@ public class FormServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        resp.setContentType("text/html");
         //los nombres que van dentro del getParameter deben ser los mismos que estan en el "name" del html
         String userName = req.getParameter("username");
         String password = req.getParameter("password");
@@ -24,10 +27,36 @@ public class FormServlet extends HttpServlet {
         String[] languagesProgramming = req.getParameterValues("languages");
         String[] roles = req.getParameterValues("roles");
         String language = req.getParameter("language");
-        String enabled = req.getParameter("enabled");
+        boolean enabled = req.getParameter("enabled") != null &&
+                                                  req.getParameter("enabled").contains("on");
         String secret = req.getParameter("secret");
+        //Validacion que general de los campos anteriores
+        List<String> errors = new ArrayList<>();
 
-        resp.setContentType("text/html");
+        //isBlank es mas completo que is.empty, porque cubre vacios y en blanco
+        if (userName == null || userName.isBlank()) {
+            errors.add("Username cannnot be null or empty");
+        }
+        if (password == null || password.isBlank()) {
+            errors.add("Password cannnot be null or empty");
+        }
+        if (email == null || email.isBlank() || !email.contains("@")) {
+            errors.add("Email cannnot be null or empty and must include '@'");
+        }
+        if (country == null || country.isBlank()) {
+            errors.add("Username cannnot be null or empty");
+        }
+        if (languagesProgramming == null || languagesProgramming.length == 0) {
+            errors.add("Languages programming cannnot be null or empty");
+        }
+        if (roles == null || roles.length == 0) {
+            errors.add("You must select at least one Rol");
+        }
+        //Solo con null, ya que es un radio button, es decir, unica opcion
+        //Por lo que no aplica vacio
+        if (language == null) {
+            errors.add("You must select one language");
+        }
 
         try (PrintWriter out = resp.getWriter()) {
 
@@ -35,31 +64,50 @@ public class FormServlet extends HttpServlet {
             out.println("<html>");
             out.println("<head>");
             out.println("<meta charset=\"UTF-8\">");
+            out.println("       <title></title>");
+            out.println("   </head>");
+            out.println("   <body>");
+            out.println("       <h1></h1>");
+            out.println("   </body>");
+            out.println("</html>");
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<meta charset=\"UTF-8\">");
             out.println("       <title>Result Form</title>");
             out.println("   </head>");
             out.println("   <body>");
-            out.println("       <h1>Result Form!</h1>");
-            out.println("       <ul>");
-            out.println("          <li>Username : " + userName + "</li>");
-            out.println("          <li>Password : " + password + "</li>");
-            out.println("          <li>Email : " + email + "</li>");
-            out.println("          <li>Country : " + country + "</li>");
+            out.println("     <h1>Result Form!</h1>");
 
-            out.print("          <li>Language(s) Programming: <ul>");
-            Arrays.asList(languagesProgramming).forEach(lang ->
-            out.println(                            "<li>" + lang + "</li>"));
-            out.print("                          </ul></li>");
+            if (errors.isEmpty()) {
+                out.println("     <ul>");
+                out.println("          <li>Username : " + userName + "</li>");
+                out.println("          <li>Password : " + password + "</li>");
+                out.println("          <li>Email : " + email + "</li>");
+                out.println("          <li>Country : " + country + "</li>");
 
-            out.print("          <li>Language(s): <ul>");
-            Arrays.asList(roles).forEach(role ->
-            out.println(                             "<li>" + role + "</li>"));
-            out.print("                              </ul></li>");
+                out.print("          <li>Language(s) Programming: <ul>");
+                Arrays.asList(languagesProgramming).forEach(lang ->
+                        out.println("<li>" + lang + "</li>"));
+                out.print("                          </ul></li>");
 
-            out.println("                        </ul>");
+                out.print("          <li>Language(s): <ul>");
+                Arrays.asList(roles).forEach(role ->
+                        out.println("<li>" + role + "</li>"));
+                out.print("                          </ul></li>");
 
-            out.println("          <li>Language: " + language + "</li>");
-            out.println("          <li>Enabled: " + enabled + "</li>");
-            out.println("          <li>Secret: " + secret + "</li>");
+
+                out.println("          <li>Language: " + language + "</li>");
+                out.println("          <li>Enabled: " + enabled + "</li>");
+                out.println("          <li>Secret: " + secret + "</li>");
+            } else {
+                errors.forEach(error -> {
+                    out.println("<li>" + error + "</li>");
+                });
+                out.println("<p><a href= \"/webapp-form/index.html\">Return form</a></p>");
+            }
+
+            out.println("      </ul>");
 
             out.println("   </body>");
             out.println("</html>");
